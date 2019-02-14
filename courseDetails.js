b2034c64d01ca3baf0b027235a45b89b6e8d298d
Akaiyo <akaiyo@users.noreply.github.com>
2019-02-14 14:42:09 +0100

    Fix bug caused by manual typescript to javascript conversion

--- courseDetails.js
+++ courseDetails.js
@@ -3,7 +3,7 @@ const bulletList = document.querySelector("form > ul.bulletList");
 if (header != null && bulletList != null) {
     const rawH1 = header.textContent.trim();
     const firstNewLine = rawH1.indexOf('\n');
-    const lvaName = rawH1.substring(rawH1.indexOf(' '));
+    let lvaName = rawH1.substring(rawH1.indexOf(' '));
     if (firstNewLine !== -1) {
         lvaName = lvaName.substring(0, firstNewLine);
     }
